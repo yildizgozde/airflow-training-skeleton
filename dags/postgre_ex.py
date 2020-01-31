c554bec5e385cdf9c3a6bfb836ccdfe83a1fcc77
Gozde Yildiz <gyildiz@schubergphilis.com>
2020-01-31 16:06:53 +0100

    days added

--- dags/postgre_ex.py
+++ dags/postgre_ex.py
@@ -4,9 +4,11 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 
-
-
-with DAG(**args) as dag:
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+with DAG(dag_id='postgres_example',default_args=args) as dag:
     pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
         task_id="postgres_to_gcs",
         sql="SELECT * FROM land_registry_price_paid_uk ""LIMIT 10”",
