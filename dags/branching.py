3b9a1a38fca216afdfee66d299dae8a249b62e3f
Gozde Yildiz <gyildiz@schubergphilis.com>
2020-01-31 14:47:12 +0100

    days added

--- dags/branching.py
+++ dags/branching.py
@@ -3,11 +3,15 @@ from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import BranchPythonOperator
+from airflow.operators.python_operator import PythonOperator
 
 import datetime
 
-def _get_weekday(execution_date, **context):
+def _print_weekday(execution_date, **context):
     print(execution_date.strftime("%a"))
+
+def _get_weekday(execution_date, **context):
+    return execution_date.strftime("%a")
 
 args = {
     'owner': 'Airflow',
@@ -17,9 +21,10 @@ weekdays_person_to_email={0: "Bob", 1: "Joe", 2: "Alice", 3: "Joe", 4: "Alice", 
 days = ["Mon", "Tue", "Wed","Thu", "Fri", "Sat", "Sun"]
 
 with DAG(dag_id='branching', default_args=args,) as dag:
+    print_days = PythonOperator(task_id="print_weekdays", python_callable=_print_weekday, provide_context=True,)
     branching = BranchPythonOperator(task_id="branching", python_callable=_get_weekday, provide_context=True,)
     for day in days:
         branching >> DummyOperator(task_id="email" + weekdays_person_to_email.get(days.index(day)))
     join = DummyOperator(task_id="final_task",trigger_rule="none_failed")
 
-branching >> join
+print_days >> branching >> join
