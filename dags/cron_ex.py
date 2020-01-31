4cbcb24f735d459105e552cd3d85efba2afcb938
Gozde Yildiz <gyildiz@schubergphilis.com>
2020-01-31 12:01:05 +0100

    cron version added

---
+++ dags/cron_ex.py
@@ -0,0 +1,24 @@
+
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+import datetime
+
+
+schedule_interval=
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+with DAG(dag_id='airflow_cron_ex', default_args=args, start_date=datetime.datetime(2020,1,31),schedule_interval="'45 13 * * MON,WED,FRI'",) as dag:
+    t1 = DummyOperator(task_id="task1")
+    t2 = DummyOperator(task_id="task2")
+    t3 = DummyOperator(task_id="task3")
+    t4 = DummyOperator(task_id="task4")
+    t5 = DummyOperator(task_id="task5")
+
+t1 >> t2 >> [t3, t4] >> t5
+
