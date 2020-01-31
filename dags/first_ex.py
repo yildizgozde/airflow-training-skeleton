ae4b0d04e4e75991944e855da5f60b45e26722b1
Gozde Yildiz <gyildiz@schubergphilis.com>
2020-01-31 11:18:23 +0100

    first commit

--- dags/first_ex.py
+++ dags/first_ex.py
@@ -9,19 +9,11 @@ args = {
     'start_date': airflow.utils.dates.days_ago(2),
 }
 
-dag = DAG(
-    dag_id='airflow_first_ex',
-    default_args=args,
-  schedule_interval=None)
-
-
-
-
-
-t1 = DummyOperator(task_id="task1", dag=dag)
-t2 = DummyOperator(task_id="task2", dag=dag)
-t3 = DummyOperator(task_id="task3", dag=dag)
-t4 = DummyOperator(task_id="task4", dag=dag)
-t5 = DummyOperator(task_id="task5", dag=dag)
+with DAG (dag_id='airflow_first_ex', default_args=args, schedule_interval=None) as dag:
+    t1 = DummyOperator(task_id="task1")
+    t2 = DummyOperator(task_id="task2")
+    t3 = DummyOperator(task_id="task3")
+    t4 = DummyOperator(task_id="task4")
+    t5 = DummyOperator(task_id="task5")
 
 t1 >> t2 >> [t3, t4] >> t5
