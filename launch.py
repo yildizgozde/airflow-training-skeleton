1d08471146838714c1a697a6efb51d088a362d6b
Gozde Yildiz <gyildiz@schubergphilis.com>
2020-02-14 11:07:29 +0100

    teset

--- launch.py
+++ launch.py
@@ -25,19 +25,18 @@ class LaunchLibraryOperator(BaseOperator):
     @apply_defaults
     def __init__(self,t1,t2,*args,**kwargs):
         super(LaunchLibraryOperator, self).__init__(*args, **kwargs)
-        result_path = "/Users/gyildiz/Desktop/3stweek/airflow-training-skeleton"  
+        self.result_path = "/Users/gyildiz/Desktop/3stweek/airflow-training-skeleton"  
     def execute(self, context):
         hook = HttpHook(self.t1, self,t2)
-        with open(posixpath.join(result_path, "launches.json"), "w") as f:
-            f.write(hook)
+        with open(posixpath.join(self.result_path, "launches.json"), "w") as f:
+            f.write(hook)
 
 
 download_rocket_launches = LaunchLibraryOperator(
     task_id="download_rocket_launches", 
-    conn_id="launchlibrary", 
-    endpoint="launch", 
-    params={"startdate": :{{t1}}}, "enddate": "{{t2}}"}, 
-    result_bucket="mydata", 
+    #params={"startdate":"{{ ds }}", "enddate": "{{ tomorrow_ds }}"}, 
+    t1 = "2015-03-20",
+    t2 = "2015-05-05"
     result_key="/data/rocket_launches/ds={{ds}}/launches.json",
     dag=dag}
 
