a04e0afbc00207a205c3decc1c11924a1fa0bbde
Gozde Yildiz <gyildiz@schubergphilis.com>
2020-02-14 11:00:44 +0100

    teset

--- launch.py
+++ launch.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3.7
-import json
-import pathlib
-import posixpath
-import airflow
-import requestsfrom airflow.models 
-import DAG
+import json
+import pathlib
+import posixpath
+import airflow
+import requests
+from airflow.models import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.http_operator import SimpleHttpOperator
 
 
 
-args = {"owner": "Gozde", "start_date": airflow.utils.dates.days_ago(1)}
+args = {"owner": "Gozde", "start_date": airflow.utils.dates.days_ago(1)}
 dag = DAG(dag_id="fetch_data_launchers", default_args=args, schedule_interval="0 0 * * *")
 
 class HttpHook(BaseHook):
@@ -23,14 +23,13 @@ class HttpHook(BaseHook):
 
 class LaunchLibraryOperator(BaseOperator):
     @apply_defaults
-    def __init__(self,*args,**kwargs, t1,t2):
+    def __init__(self,t1,t2,*args,**kwargs):
         super(LaunchLibraryOperator, self).__init__(*args, **kwargs)
         result_path = "/Users/gyildiz/Desktop/3stweek/airflow-training-skeleton"  
     def execute(self, context):
         hook = HttpHook(self.t1, self,t2)
-        hook()
         with open(posixpath.join(result_path, "launches.json"), "w") as f:
-            f.write(response.text)
+            f.write(hook)
 
 
 download_rocket_launches = LaunchLibraryOperator(
