4146f76eba297f4eb5f5daadcc91eb863e3517cf
Gozde Yildiz <gyildiz@schubergphilis.com>
2020-02-14 11:46:53 +0100

    test

--- dags/launch.py
+++ dags/launch.py
@@ -38,7 +38,7 @@ class LaunchLibraryOperator(BaseOperator):
     def execute(self, context):
         hook = HttpHook(self.t1, self.t2)
         with open("launches.json", "w+t") as f:
-            f.write(hook.get_results())
+            f.write(json.dumps(hook.get_results()))
         GoogleCloudStorageHook(google_cloud_storage_conn_id='google_conn_default').upload(bucket="launchbucket",object="launches.json",filename="launches.json")
         
 
